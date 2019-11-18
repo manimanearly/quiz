a0b1a5fc6731834e604f4be8d02b55ba73e5fdef
Hazuki Arai <manimani23230139@yahoo.co.jp>
2019-11-18 15:39:32 +0900

    validation in qustiondata.php

--- qustiondata.php
+++ qustiondata.php
@@ -4,6 +4,11 @@
 	$qid = $_GET['qid'];
 	$answer = $_GET['answer'];
 	
+	if($qid == -1 || !is_numeric($qid) || !((0<=$qid) && ($qid<count($question)))){
+		echo 'エラー： $qid invalid';
+		exit(1);
+	}
+	
 	if($question[$qid][1] == $answer){
 		echo "正解";
 	}
